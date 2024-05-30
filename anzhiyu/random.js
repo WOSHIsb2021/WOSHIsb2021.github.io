22f17e5f3162f94d0a5225fda98667a0c8013f77
WOSHIsb2021 <3186117270@qq.com>
2024-05-30 14:39:17 +0800

    Site updated: 2024-05-30 14:39:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/13/css/","2024/05/13/CSS-1/","2024/05/09/hello-world/","2024/05/10/new-world/","2024/05/11/text/","2024/05/10/使用hexo创建个人博客/","2024/05/12/关于未来/","2024/05/23/日记/"];function toRandomPost(){
+var posts=["2024/05/13/css/","2024/05/13/CSS-1/","2024/05/09/hello-world/","2024/05/10/new-world/","2024/05/11/text/","2024/05/12/关于未来/","2024/05/23/日记/","2024/05/10/使用hexo创建个人博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
